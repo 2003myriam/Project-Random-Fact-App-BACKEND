3819e7c080b9aa16da3dae49baffdc7514395def
2003myriam <lounesmyriam210@gmail.com>
2026-05-14 13:23:56 +0100

    creating fact and book models

---
+++ src/Models/FactModel.js
@@ -0,0 +1,14 @@
+const mongoose =require("mongoose")
+
+const factSchema=new mongoose.Schema({
+  fact:{type: String , required:true},
+  created_at:{type:Date , default:Date.now()},
+
+  /* one book to many articles so id of the one in the many  */
+  BookId:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"Book"
+    }
+})
+const Fact=mongoose.model("Fact",factSchema)
+module.exports = {Fact}
