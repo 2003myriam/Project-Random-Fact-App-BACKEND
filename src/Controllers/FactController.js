24c1519949da97027821cfee66b0faa3c4f8a978
2003myriam <lounesmyriam210@gmail.com>
2026-05-15 17:49:36 +0100

    create random fact using  Math.random

--- src/Controllers/FactController.js
+++ src/Controllers/FactController.js
@@ -19,6 +19,29 @@ class  FactController{
     res.status(500).json(error.message)
   }
 }
+async AllCitationOfSameBook(req,res){
+  try {
+  console.log(req.body);
+ 
+
+  
+/* _______Avoir toutes les citations du meme livre________ */
+const bookId = req.params.BookId;
+const findbookcitation = await Fact.find({ BookId: bookId });
+/* _______Avoir a random citation a chaque clique dans ce livre_______ */
+const randomItem = findbookcitation[Math.floor(Math.random() * findbookcitation.length)];
+
+  res.json({
+    "message":" the citation is :",
+    data:
+        randomItem.citation
+    
+  })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error.message)
+  }
+}
  
 }
 module.exports=new FactController
--- src/Routes/FactRoute.js
+++ src/Routes/FactRoute.js
@@ -5,4 +5,9 @@ const router =express.Router();
 const FacatControllers = require("../Controllers/FactController");
 
 router.post("/citation", FacatControllers.addFact)
+router.get("/findallcitation/:BookId/random",FacatControllers.AllCitationOfSameBook);
 module.exports= router;
+
+
+/*   const value = Math.random(); 
+    res.json({ randomNumber: value }); */
